2fe91cbed817900435a22c4985190ec5035f9f8f
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2018-05-02 01:35:24 -0300

    deploy

---
+++ assets/js/2.e7eff608.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{57:function(n,t,e){},61:function(n,t,e){"use strict";var c=e(57);e.n(c).a},62:function(n,t,e){"use strict";e.r(t);e(61);var c=e(0),i=Object(c.a)({},function(){var n=this.$createElement,t=this._self._c||n;return t("div",{staticClass:"container"},[t("vue-nixie-clock")],1)},[],!1,null,"02ae09fe",null);t.default=i.exports}}]);
