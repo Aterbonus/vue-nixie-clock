4caac8efe4e5bfe0f14b59cdf8ea5b9f4b606c48
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2018-05-17 00:21:56 -0400

    build(release script): release script accepts version bump type

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-BUMP=$(node_modules/.bin/conventional-recommended-bump -p angular)
+BUMP=${1:-$(node_modules/.bin/conventional-recommended-bump -p angular)}
 
 echo "Using npm version..."
 npm version --message "chore(release): %s" $BUMP
